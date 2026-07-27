98e16a6db0b402a29dc2cd5ba6bdfa52863826bb
Priyansh Kumar <pkd09052006@gmail.com>
2026-07-28 00:23:42 +0530

    Fix: Node CI fixed

---
+++ backend/index.js
@@ -0,0 +1,6 @@
+// const express = require('Express');
+// const mongodb = require('mongodb');
+
+const http = require('http');
+
+const newServer = http.createServer((req, res) => {console.log("Server started!")});
